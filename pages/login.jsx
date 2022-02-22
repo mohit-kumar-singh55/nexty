3457a7ce0f9a6412589338a69bb9ce1c819e8ee4
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-22 18:51:35 +0530

    bug bug bug bug!!!!

--- pages/login.jsx
+++ pages/login.jsx
@@ -2,13 +2,30 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons";
-import shareVideo from "../public/assets/share.mp4";
 import logo from "../public/assets/logowhite.png";
+import Image from 'next/image';
 
 function Login() {
     return (
-        <div>
-            <video src={shareVideo} autoPlay muted loop />
+        <div className='flex justify-start items-center flex-col h-screen'>
+            <div className="relative w-full h-full">
+                <div className="w-full h-full custom-bg" />
+                <div className="absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0">
+                    <div className="p-5">
+                        <Image src={logo} alt="Logo" />
+                    </div>
+                    <div className="shadow-2xl">
+                        <GoogleLogin
+                            clientId=''
+                            render={(renderProps) => (
+                                <button type='button' className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'>
+                                    <FcGoogle className="mr-4" />SignIn with Google
+                                </button>
+                            )}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

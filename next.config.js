220f792f3e661a85191649fe470705a39afea49c
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-02-22 20:24:07 +0530

    login complete

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ module.exports = {
   //   domains: ['media.graphcms.com'],
   // },
   env: {
-    REACT_GOOGLE_CLIENT_ID: '211652113887 - ak98aojvmtqsk2hf4gr878pdr1rpe06k.apps.googleusercontent.com'
+    REACT_GOOGLE_CLIENT_ID: '211652113887 - ak98aojvmtqsk2hf4gr878pdr1rpe06k.apps.googleusercontent.com',
+    REACT_PROJECT_ID: '0uqqh7gb',
+    REACT_SANITY_TOKEN: 'skPF4MLtAZ69yyhNtL7PQhkZEdA2YDm7KlthRT5uXuKUBQ9HR5bEyGpPcAYFV2DEqFmjnipZmGX4UZ2YHf9Cabt51wa2BAgO6MnPhZ8mvpIJGdJXL9gGmGxr6VXseiaPT5QVBhoimPbecyOip6BIhOly7maO0PirG3RR2uUi3VCda86HVxG3',
   }
 }
--- pages/login.jsx
+++ pages/login.jsx
@@ -4,8 +4,11 @@ import { useRouter } from "next/router";
 // import FcGoogle from "react-icons";
 import logo from "../public/assets/logowhite.png";
 import Image from 'next/image';
+import { client } from "../client";
 
 function Login() {
+    const router = useRouter();
+
     const responseGoogle = (response) => {
         localStorage.setItem('user', JSON.stringify(response.profileObj));
 
@@ -17,11 +20,15 @@ function Login() {
             userName: name,
             image: imageUrl
         }
+
+        client.createIfNotExists(doc)
+            .then(() => {
+                router.replace("/")
+            })
     }
 
     return (
         <div className='flex justify-start items-center flex-col h-screen'>
-            {console.log(process.env.REACT_GOOGLE_CLIENT_ID)}
             <div className="relative w-full h-full">
                 <div className="w-full h-full custom-bg" />
                 <div className="absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0">
@@ -31,16 +38,15 @@ function Login() {
                     <div className="shadow-2xl">
                         <GoogleLogin
                             clientId={process.env.REACT_GOOGLE_CLIENT_ID}
-                            // render={(renderProps) => (
-                            //     <button
-                            //         type='button'
-                            //         className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
-                            //         onClick={renderProps.onClick}
-                            //         disabled={renderProps.disabled}>
-                            //         <FcGoogle className="mr-4" />Sign in with Google
-                            //     </button>
-                            // )}
-                            className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                            render={(renderProps) => (
+                                <button
+                                    type='button'
+                                    className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}>
+                                    Sign in with Google
+                                </button>
+                            )}
                             onSuccess={responseGoogle}
                             onFailure={responseGoogle}
                             cookiePolicy="single_host_origin"
